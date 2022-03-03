95919f7412d5a5fc6a7b7fb1e04ef19e044cbeb4
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-03-03 14:29:55 +0000

    add routes for requests(so far dummy version info)

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const routes = require('./routes/routes');
+
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Beebop!'
-    });
-});
+routes(app)
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
---
+++ routes/routes.js
@@ -0,0 +1,23 @@
+const router = app => {
+    app.get('/', (request, response) => {
+        response.json({
+            message: 'Welcome to beebop!'
+        });
+    });
+    app.get('/version', (request, response) => {
+        response.send(version_info);
+    });
+}
+
+
+
+
+const version_info = {
+    "status":"success",
+    "errors":[],
+    "data":[{"name":"beebop","version":"0.1.0"},{"name":"poppunk","version":"2.4.0"}]
+    
+}
+
+// Export the router
+module.exports = router;
