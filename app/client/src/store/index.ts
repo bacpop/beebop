6a31ec1b144cd9be116d43418f870f0b0fa5d9bd
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-03-07 15:33:33 +0000

    implemented VUEX to handle version info

--- app/client/src/store/index.ts
+++ app/client/src/store/index.ts
@@ -1,13 +1,23 @@
 import { createStore } from 'vuex';
+import config from '@/resources/config.json';
 
 export default createStore({
   state: {
+    versions: [],
   },
   getters: {
   },
   mutations: {
+    setVersions(state, versioninfo) {
+      state.versions = versioninfo;
+    },
   },
   actions: {
+    async getVersions({ commit }) {
+      const response = await fetch(`${config.app_url}/version`);
+      const data = await response.json();
+      commit('setVersions', data.data);
+    },
   },
   modules: {
   },
