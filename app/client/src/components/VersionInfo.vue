f4eacde6ea68646a911fa7319b95874f2e08083f
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-03-07 15:35:26 +0000

    implemented VUEX to handle version info, separate url from code

--- app/client/src/components/VersionInfo.vue
+++ app/client/src/components/VersionInfo.vue
@@ -3,36 +3,27 @@
     <p>
       Version info:
     </p>
-    <p v-for="component in versioninfo" :key="component.name">{{component.name}}
+    <p v-for="component in versions" :key="component.name">{{component.name}}
         {{component.version}}</p>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
-const API_URL = 'http://localhost:4000';
+import { mapState, mapActions } from 'vuex';
 
 export default defineComponent({
   name: 'VersionInfo',
-  data() {
-    return {
-      versioninfo: [],
-    };
-  },
   mounted() {
     this.getVersions();
   },
   methods: {
-    async getVersions() {
-      try {
-        const response = await fetch(`${API_URL}/version`);
-        const data = await response.json();
-        this.versioninfo = data.data;
-      } catch (error) {
-        console.error(error);
-      }
-    },
+    ...mapActions([
+      'getVersions',
+    ]),
   },
+  computed: mapState([
+    'versions',
+  ]),
 });
 </script>
