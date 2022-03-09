7a129d62cc8285b10a5aafb6c4db2bc4dd4ef537
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-03-09 17:09:52 +0000

    Added axios mock adapter to make actions test independent of express router

--- app/client/tests/unit/actions.spec.ts
+++ app/client/tests/unit/actions.spec.ts
@@ -1,8 +1,15 @@
 import actions from "@/store/actions";
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import config from '../../src/resources/config.json';
 const { version_info } = require('../../../server/routes/routes');
 
-describe("version info", () => {
-    it("get version info", async () => {
+describe("Actions", () => {
+    const mock = new MockAdapter(axios);
+
+    mock.onGet(`${config.app_url}/version`).reply(200, version_info);
+
+    it("getVersions", async () => {
         const commit = jest.fn()
         await actions.getVersions({ commit })
 
