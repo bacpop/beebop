689c9aafe8daf1399944864955174064684a4707
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-03-21 11:22:14 +0000

    added axios mock adapter to backend tests

--- app/server/tests/integration/router.test.ts
+++ app/server/tests/integration/router.test.ts
@@ -1,16 +1,23 @@
 import request from 'supertest';
 import express from "express";
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 import { router } from '../../src/routes/routes';
-import * as versionInfo from '../../../server/resources/versionInfo.json';
+import versionInfo from '../../../server/resources/versionInfo.json';
+import config from '../../src/resources/config.json';
 
 const app = express();
 
 router(app)
 
 describe("testing-server-routes", () => {
+    const mock = new MockAdapter(axios);
+    mock.onGet(`${config.flask_url}/version`).reply(200, versionInfo);
+
     it("GET /version", async () => {
-        const { body } = await request(app).get("/version");
-        expect(body).toEqual(versionInfo);
+        const response= await request(app).get("/version");
+        console.log(versionInfo)
+        expect(response.body).toMatchObject(versionInfo);
     });
     it("GET /", async () => {
         const { body } = await request(app).get("/");
