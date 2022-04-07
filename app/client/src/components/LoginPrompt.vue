6d76b3021436802920ea8fa0f60bb87852663a2c
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-04-07 16:45:46 +0100

    Add user Login/Logout button to home screen

---
+++ app/client/src/components/LoginPrompt.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class=social>
+    <ul>
+      <li><a href=http://localhost:4000/login/google class="btn btn-block btn-social btn-google"><i class='fa-brands fa-google'></i> Login with Google</a></li>
+      <li><a href=http://localhost:4000/login/github class="btn btn-block btn-social btn-github"><i class='fa-brands fa-github'></i> Login with Github</a></li>
+    </ul>
+  </div>
+</template>
+
+<style>
+  @import 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css';
+  @import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css';
+  @import '../CSS/bootstrap-social.css';
+
+  .social ul {
+    margin: 0;
+    padding: 5px;
+    text-align:center;
+    display: inline-block;
+  }
+
+  .social ul li {
+    margin: 10px;
+    list-style: none outside none;
+    width:8cm;
+  }
+</style>
--- app/client/src/views/HomeView.vue
+++ app/client/src/views/HomeView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="home">
+  <div class='home'>
     <h1>Welcome to beebop!</h1>
+    <LoginPrompt v-if='!user' />
+    <GreetingAndLogout v-if='user' />
   </div>
 </template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+import { mapState, mapActions } from 'vuex';
+import LoginPrompt from '@/components/LoginPrompt.vue';
+import GreetingAndLogout from '@/components/GreetingAndLogout.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    LoginPrompt,
+    GreetingAndLogout,
+  },
+  mounted() {
+    this.getUser();
+  },
+  methods: {
+    ...mapActions(['getUser']),
+  },
+  computed: mapState(['user']),
+});
+</script>
