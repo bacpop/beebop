ec206252a4506918c0e1ee9facd86ee3eda8f137
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-04-08 13:42:10 +0100

    added auth Check

--- app/server/src/routes/routes.ts
+++ app/server/src/routes/routes.ts
@@ -2,26 +2,40 @@ import axios from 'axios';
 import passport from 'passport';
 import config from '../resources/config.json';
 
+const authCheck = (req, res, next) => {
+    if (!req.user) {
+        res.json(false);
+    } else {
+        next();
+    }
+}
+
 export const router = (app => {
-    app.get('/', (request, response) => {
-        response.json({
-            message: 'Welcome to beebop!'
-        });
-    });
+    app.get('/',
+        (request, response) => {
+            response.json({
+                message: 'Welcome to beebop!'
+            });
+        }
+    );
 
-    app.get('/version', getVersionInfo);
+    app.get('/version',
+        getVersionInfo);
 
-    app.get('/login/google', passport.authenticate('google', { scope: ['profile'] }));
+    app.get('/login/google',
+        passport.authenticate('google', { scope: ['profile'] }));
 
-    app.get('/login/github', passport.authenticate('github', { scope: ['profile'] }));
+    app.get('/login/github',
+        passport.authenticate('github', { scope: ['profile'] }));
 
-    app.get('/user', (request, response) => {
-        if (request.user) {
-            if(request.user.provider=='github'){
+    app.get('/user',
+        authCheck,
+        (request, response) => {
+            if (request.user.provider == 'github') {
                 response.json({
                     id: request.user.id,
                     provider: request.user.provider,
-                    name : request.user.username
+                    name: request.user.username
                 });
             } else {
                 response.json({
@@ -30,25 +44,29 @@ export const router = (app => {
                     name: request.user.name.givenName
                 });
             }
-        } else {
-            response.json(false);
         }
-    });
+    );
 
-    app.get('/logout', (req, res) => {
-        req.logout();
-        res.redirect(config.client_url);
-    });
+    app.get('/logout',
+        (req, res) => {
+            req.logout();
+            res.redirect(config.client_url);
+        }
+    );
 
-    app.get('/return/google', passport.authenticate('google', { failureRedirect: '/' }),
+    app.get('/return/google',
+        passport.authenticate('google', { failureRedirect: '/' }),
         (req, res) => {
             res.redirect(config.client_url);
-    });
+        }
+    );
 
-    app.get('/return/github', passport.authenticate('github', { failureRedirect: '/' }),
+    app.get('/return/github',
+        passport.authenticate('github', { failureRedirect: '/' }),
         (req, res) => {
             res.redirect(config.client_url);
-    });
+        }
+    );
 })
 
 export async function getVersionInfo(request, response) {
