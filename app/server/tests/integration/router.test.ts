0a87ca91d0dae720378d6410d64dc6f461c210fd
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-04-11 11:33:17 +0100

    added unit tests using passport mock auth

--- app/server/tests/integration/router.test.ts
+++ app/server/tests/integration/router.test.ts
@@ -1,13 +1,16 @@
 import request from 'supertest';
+import Agent from'supertest';
 import express from "express";
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { router } from '../../src/routes/routes';
+import { configApp } from '../../src/configApp';
 import versionInfo from '../../../server/resources/versionInfo.json';
 import config from '../../src/resources/config.json';
 
-const app = express();
 
+const app = express();
+configApp(app)
 router(app)
 
 describe("testing-server-routes", () => {
@@ -22,5 +25,28 @@ describe("testing-server-routes", () => {
     it("GET /", async () => {
         const { body } = await request(app).get("/");
         expect(body).toEqual({ message: 'Welcome to beebop!' });
+    });
+    it("GET /user", async () => {
+        const { body } = await request(app).get("/user");
+        expect(body).toEqual(false);
+    });
+});
+
+describe("testing-server-routes as logged in user", () => {
+    let agent;
+    beforeEach(() => {
+        agent = Agent.agent(app);
+      });
+
+    it('should authenticate the user', () => {
+        return agent.get('/login/mock').expect(200, { status: 'ok' });
+    });
+
+    it("GET /user", async () => {
+        Agent.agent
+        await agent.get('/login/mock');
+        const { body } = await agent.get("/user");
+        console.log(body)
+        expect(body).toEqual({id:'1234', provider:'mock', name:'Foo'});
     });
 });
