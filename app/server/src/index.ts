f6303054c73180d9440956e56dcb0603b0cc934b
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-04-11 11:34:30 +0100

    separated app configuration to use for tests

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -1,68 +1,12 @@
 import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import passport from 'passport';
-import session from 'express-session';
-import PassportGoogle from 'passport-google-oauth20';
-import PassportGithub from 'passport-github';
 
-import config from './resources/config.json';
+import { configApp } from './configApp';
 import { router } from './routes/routes';
+import config from './resources/config.json';
 
 const app = express();
 
-//set up passport
-passport.use(
-  new PassportGoogle.Strategy(
-    {
-      clientID: config.GOOGLE_CLIENT_ID,
-      clientSecret: config.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/return/google'
-    },
-    (accessToken, refreshToken, profile, cb) => {
-      return cb(null, profile);
-    }
-  )
-);
-
-passport.use(
-  new PassportGithub.Strategy({
-      clientID: config.GITHUB_CLIENT_ID,
-      clientSecret: config.GITHUB_CLIENT_SECRET,
-      callbackURL: '/return/github'
-    },
-    (accessToken, refreshToken, profile, cb) => {
-      return cb(null, profile);
-    }
-  )
-);
-
-
-passport.serializeUser((user, cb) => {
-  cb(null, user);
-});
-
-passport.deserializeUser((obj, cb) => {
-  cb(null, obj);
-});
-
-app.use(session({ secret: config.SESSION_SECRET, cookie: { secure: false }, resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(morgan('tiny'));
-app.use(
-  cors({
-    origin: [
-      config.client_url
-    ],
-    credentials: true,
-  })
-);
-app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+configApp(app)
 
 router(app)
 
