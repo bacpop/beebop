68a867092356a775dbd8e7ae73dbefceb334d70f
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-04-12 14:07:37 +0100

    import scss styles with npm and include at top level component

--- app/client/src/components/GreetingAndLogout.vue
+++ app/client/src/components/GreetingAndLogout.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <p>Nice to see you, {{user.data.name}}!</p>
-    <a href=http://localhost:4000/logout class="btn-logout">Logout</a>
+    <a :href="generateLogout" class="btn btn-block btn-logout">Logout</a>
   </div>
 </template>
 
 <script lang='ts'>
 import { defineComponent } from 'vue';
 import { mapState, mapActions } from 'vuex';
+import config from '@/resources/config.json';
 
 export default defineComponent({
   name: 'GreetingAndLogout',
@@ -17,33 +18,11 @@ export default defineComponent({
   methods: {
     ...mapActions(['getUser']),
   },
-  computed: mapState(['user']),
+  computed: {
+    generateLogout() {
+      return `${config.server_url}/logout`;
+    },
+    ...mapState(['user']),
+  },
 });
 </script>
-
-<style scoped>
-  .btn-logout{
-    color:#fff;
-    background-color:#9099a0;
-    border-color:rgba(0,0,0,0.2);
-    padding:6px 12px;
-    line-height:40px;
-    font-size:1.3em;
-    text-align:center;
-    font-weight:400;
-    text-align:center;
-    vertical-align:middle;
-    cursor:pointer;
-    border:1px solid transparent;
-    border-radius:4px;
-    text-decoration: none;
-  }
-
-  .btn-logout:hover{
-    color:#fff;
-    background-color:#42b983;
-    border-color:rgba(0,0,0,0.2);
-    text-decoration: none;
-  }
-
-</style>
--- app/client/src/components/LoginPrompt.vue
+++ app/client/src/components/LoginPrompt.vue
@@ -2,9 +2,9 @@
   <div class=social>
     <ul>
       <li><a :href="generateLoginGoogle" class="btn btn-block btn-social btn-google">
-        <i class='fa-brands fa-google'></i> Login with Google</a></li>
+        <i class='fa fa-google'></i> Login with Google</a></li>
       <li><a :href="generateLoginGithub" class="btn btn-block btn-social btn-github">
-        <i class='fa-brands fa-github'></i> Login with Github</a></li>
+        <i class='fa fa-github'></i> Login with Github</a></li>
     </ul>
   </div>
 </template>
@@ -26,9 +26,7 @@ export default defineComponent({
 </script>
 
 <style>
-  @import 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css';
-  @import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css';
-  @import '../CSS/bootstrap-social.css';
+  @import '../../node_modules/bootstrap-social/bootstrap-social.css';
 
   .social ul {
     margin: 0;
