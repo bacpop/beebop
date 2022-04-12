be779ba0b5aa65f052dd73c655106c6f1fe631ee
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-04-12 10:28:54 +0100

    replaced hardcoded url by computed url

--- app/client/src/components/LoginPrompt.vue
+++ app/client/src/components/LoginPrompt.vue
@@ -1,11 +1,29 @@
 <template>
   <div class=social>
     <ul>
-      <li><a href=http://localhost:4000/login/google class="btn btn-block btn-social btn-google"><i class='fa-brands fa-google'></i> Login with Google</a></li>
-      <li><a href=http://localhost:4000/login/github class="btn btn-block btn-social btn-github"><i class='fa-brands fa-github'></i> Login with Github</a></li>
+      <li><a :href="generateLoginGoogle" class="btn btn-block btn-social btn-google">
+        <i class='fa-brands fa-google'></i> Login with Google</a></li>
+      <li><a :href="generateLoginGithub" class="btn btn-block btn-social btn-github">
+        <i class='fa-brands fa-github'></i> Login with Github</a></li>
     </ul>
   </div>
 </template>
+
+<script>
+import { defineComponent } from 'vue';
+import config from '@/resources/config.json';
+
+export default defineComponent({
+  computed: {
+    generateLoginGoogle() {
+      return `${config.server_url}/login/google`;
+    },
+    generateLoginGithub() {
+      return `${config.server_url}/login/github`;
+    },
+  },
+});
+</script>
 
 <style>
   @import 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css';
