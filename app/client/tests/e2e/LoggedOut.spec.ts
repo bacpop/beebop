eace16e5d1950a7bf7b4a30b0478b17ad38f4d47
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-04-29 12:49:33 +0100

    Check for Login button

--- app/client/tests/e2e/LoggedOut.spec.ts
+++ app/client/tests/e2e/LoggedOut.spec.ts
@@ -1,16 +1,21 @@
 import { test, expect } from '@playwright/test';
+import config from '../../src/resources/config.json';
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:8080/');
-});
+test.describe('Logged out Tests', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(config.client_url);
+  });
 
-test.describe('Basic Tests', () => {
   test('should display greeting on start page', async ({ page }) => {
-    await expect(page.locator('.home')).toHaveText('Welcome to beebop!');
+    await expect(page.locator('.home')).toContainText('Welcome to beebop!');
   });
 
   test('should display version info when clicking on about', async ({ page }) => {
     await page.click('text=About');
     await expect(page.locator('.about:has(.version-info)')).toContainText(['beebop']);
+  });
+
+  test('should display Login button', async ({ page }) => {
+    await expect(page.locator('.auth')).toContainText('Login');
   });
 });
