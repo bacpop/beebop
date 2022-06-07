00b1ea495624644afe74c801068a4406b58bbfe2
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-06-07 14:59:33 +0100

    added new tests for new functionality

--- app/client/tests/e2e/LoggedIn.spec.ts
+++ app/client/tests/e2e/LoggedIn.spec.ts
@@ -1,4 +1,7 @@
+/// <reference lib="dom"/>
+
 import { test, expect } from '@playwright/test';
+import { readFileSync } from 'node:fs';
 import config from '../../src/resources/config.json';
 
 test.describe('Logged in Tests', () => {
@@ -14,5 +17,30 @@ test.describe('Logged in Tests', () => {
   test('should display Login buttons after Logging out', async ({ page }) => {
     await page.click('text=Logout');
     await expect(page.locator('.auth')).toContainText('Login');
+  });
+
+  test('should display Start Analysis button once files were submitted and job status once pressed)', async ({ page }) => {
+    // Read file into a buffer
+    const buffer = readFileSync('./tests/files/6930_8_11.fa');
+    // Create the DataTransfer and File
+    const dataTransfer = await page.evaluateHandle((data) => {
+      const dt = new DataTransfer();
+      const file = new File([data.toString('hex')], '6930_8_11.fa', { type: 'text/plain' });
+      dt.items.add(file);
+      return dt;
+    }, buffer);
+    // Do the same for second file
+    const buffer2 = readFileSync('./tests/files/6930_8_13.fa');
+    const dataTransfer2 = await page.evaluateHandle((data) => {
+      const dt = new DataTransfer();
+      const file = new File([data.toString('hex')], '6930_8_13.fa', { type: 'text/plain' });
+      dt.items.add(file);
+      return dt;
+    }, buffer2);
+    await page.dispatchEvent('.dropzone', 'drop', { dataTransfer });
+    await page.dispatchEvent('.dropzone', 'drop', { dataTransfer2 });
+    await expect(page.locator('.startAnalysis')).toContainText('Start Analysis');
+    await page.click('text=Start Analysis');
+    await expect(page.locator('.startAnalysis')).toContainText('Job status:');
   });
 });
--- app/client/tests/unit/HomeView.spec.ts
+++ app/client/tests/unit/HomeView.spec.ts
@@ -1,61 +1,50 @@
 import { mount } from '@vue/test-utils';
 import HomeView from '@/views/HomeView.vue';
-import { createStore } from 'vuex';
+import Vuex from 'vuex';
+import { RootState } from '@/store/state';
+import { mockRootState } from '../mocks';
 
 describe('Home', () => {
   const getUser = jest.fn();
-
-  const storeLoggedIn = createStore({
-    state() {
-      return {
-        user: {
-          status: 'success',
-          errors: [],
-          data: {
-            name: 'Jane',
-            id: '543653d45',
-            provider: 'google',
-          },
-        },
-      };
-    },
-    actions: {
-      getUser,
-    },
-  });
-
-  const storeLoggedOut = createStore({
-    state() {
-      return {
-        user: {
-          status: 'failure',
-          errors: [],
-          data: null,
-        },
-      };
-    },
-    actions: {
-      getUser,
-    },
-  });
 
   beforeEach(() => {
     jest.resetAllMocks();
   });
 
   it('calls getUser', () => {
+    const store = new Vuex.Store<RootState>({
+      state: mockRootState({
+        user: {
+          status: 'failure',
+          errors: [],
+          data: null,
+        },
+      }),
+      actions: {
+        getUser,
+      },
+    });
     mount(HomeView, {
       global: {
-        plugins: [storeLoggedOut],
+        plugins: [store],
       },
     });
     expect(getUser).toHaveBeenCalled();
   });
 
   it('shows login buttons when not logged in', () => {
+    const store = new Vuex.Store<RootState>({
+      state: mockRootState({
+        user: {
+          status: 'failure',
+          errors: [],
+          data: null,
+        },
+      }),
+    });
     const wrapper = mount(HomeView, {
       global: {
-        plugins: [storeLoggedOut],
+        plugins: [store],
       },
     });
     expect(wrapper.find('h1').text()).toMatch('Welcome to beebop!');
@@ -66,14 +55,76 @@ describe('Home', () => {
   });
 
   it('shows logout button when logged in', () => {
+    const store = new Vuex.Store<RootState>({
+      state: mockRootState({
+        user: {
+          status: 'success',
+          errors: [],
+          data: {
+            name: 'Jane',
+            id: '543653d45',
+            provider: 'google',
+          },
+        },
+      }),
+    });
     const wrapper = mount(HomeView, {
       global: {
-        plugins: [storeLoggedIn],
+        plugins: [store],
       },
     });
     expect(wrapper.find('h1').text()).toMatch('Welcome to beebop!');
-    const versions = wrapper.findAll('.btn-logout');
-    expect(versions.length).toBe(1);
-    expect(versions[0].text()).toBe('Logout');
+    const buttons = wrapper.findAll('.btn-standard');
+    expect(buttons.length).toBe(1);
+    expect(buttons[0].text()).toBe('Logout');
+  });
+
+  it('shows Start Analysis button when files were submitted', () => {
+    const store = new Vuex.Store<RootState>({
+      state: mockRootState({
+        user: {
+          status: 'success',
+          errors: [],
+          data: {
+            name: 'Jane',
+            id: '543653d45',
+            provider: 'google',
+          },
+        },
+        uploadedFiles: 3,
+      }),
+    });
+    const wrapper = mount(HomeView, {
+      global: {
+        plugins: [store],
+      },
+    });
+    const buttons = wrapper.findAll('.btn-standard');
+    expect(buttons.length).toBe(2);
+    expect(buttons[1].text()).toBe('Start Analysis');
+  });
+
+  it('shows analysis status once analysis was started', () => {
+    const store = new Vuex.Store<RootState>({
+      state: mockRootState({
+        user: {
+          status: 'success',
+          errors: [],
+          data: {
+            name: 'Jane',
+            id: '543653d45',
+            provider: 'google',
+          },
+        },
+        uploadedFiles: 3,
+        analysisStatus: 'started',
+      }),
+    });
+    const wrapper = mount(HomeView, {
+      global: {
+        plugins: [store],
+      },
+    });
+    expect(wrapper.find('h5').text()).toMatch('Job status:');
   });
 });
