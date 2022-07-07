c9f94bdb1699a60531f82d840de8f9c144bc06de
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-07-07 14:35:46 +0100

    added Emmas suggestions, new separate file to test dropzone text when isDragactive = true

--- app/client/tests/unit/DropZone.spec.ts
+++ app/client/tests/unit/DropZone.spec.ts
@@ -1,3 +1,15 @@
+const mockGetRootProps = jest.fn().mockReturnValue({ rprop: 'rval' });
+const mockGetInputProps = jest.fn().mockReturnValue({ iprop: 'ival' });
+const mockUseDropzone = jest.fn().mockReturnValue({
+  getRootProps: mockGetRootProps,
+  getInputProps: mockGetInputProps,
+  isDragActive: false,
+});
+jest.mock('vue3-dropzone', () => ({
+  useDropzone: mockUseDropzone,
+}));
+
+/* eslint-disable import/first */
 import DropZone from '@/components/DropZone.vue';
 import { mount } from '@vue/test-utils';
 import { RootState } from '@/store/state';
@@ -40,11 +52,22 @@ describe('Dropzone', () => {
   it('processes dropped files', () => {
     const file = {
       name: 'sample.fa',
-      text: () => Promise.resolve('ACGTGTAGTCTGACGTAAC'),
     };
-    // call onDrop function
+
+    expect(mockUseDropzone).toHaveBeenCalledTimes(1);
+    expect(mockUseDropzone.mock.calls[0][0].accept).toStrictEqual(['.fa', '.fasta']);
     wrapper.vm.onDrop([file]);
-    // should have called processFiles()
     expect(processFiles).toHaveBeenCalledTimes(1);
+    expect(processFiles.mock.calls[0][1]).toStrictEqual([file]);
   });
+
+  it('sets dropzone props', () => {
+    expect(wrapper.find('.dropzone').attributes('rprop')).toBe('rval');
+    expect(wrapper.find('input').attributes('iprop')).toBe('ival');
+  });
+
+  it('displays text for !isDragActive', () => {
+    expect(wrapper.find('.dropzone').text()).toBe("Drag and drop your fasta files here, or click to select files");
+  });
+
 });
