4f6da3429c98082eb3d25ab26ec1cec8c9522d22
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-07-20 14:03:18 +0100

    added test coverage and new tests

---
+++ app/client/tests/unit/store/index.spec.ts
@@ -0,0 +1,20 @@
+import index from '@/store/index'
+
+describe("store index", () => {
+    
+    it("creates a root state", () => {
+        expect(index).toHaveProperty("state");
+    });
+
+    it("has mutations", () => {
+        expect(index).toHaveProperty("_modules.root._rawModule.mutations.addFile");
+        expect(index).toHaveProperty("_modules.root._rawModule.mutations.setVersions");
+        expect(index).toHaveProperty("_modules.root._rawModule.mutations.setUser");
+    });
+
+    it("has actions", () => {
+        expect(index).toHaveProperty("_modules.root._rawModule.actions.processFiles");
+        expect(index).toHaveProperty("_modules.root._rawModule.actions.getVersions");
+        expect(index).toHaveProperty("_modules.root._rawModule.actions.getUser");
+    });
+});
