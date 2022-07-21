01e56dd986676321db6a581671cd8e8cd9ffff41
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-07-21 13:18:02 +0100

    delinted

--- app/client/tests/unit/router.spec.ts
+++ app/client/tests/unit/router.spec.ts
@@ -1,30 +1,30 @@
-import router from "@/router/index";
+import router from '@/router/index';
 
-describe("router", () => {
-    const routes = router.getRoutes();
+describe('router', () => {
+  const routes = router.getRoutes();
 
-    it("defines expected routes", () => {
-        expect(routes.length).toBe(2);
-        expect(routes[0].name).toBe("home");
-        expect(routes[0].path).toBe("/");
+  it('defines expected routes', () => {
+    expect(routes.length).toBe(2);
+    expect(routes[0].name).toBe('home');
+    expect(routes[0].path).toBe('/');
 
-        expect(routes[1].name).toBe("about");
-        expect(routes[1].path).toBe("/about");
-    });
+    expect(routes[1].name).toBe('about');
+    expect(routes[1].path).toBe('/about');
+  });
 
-    it("Home route loads Home component", () => {
-        expect(routes[0].components.default.name).toBe("HomeView");
-    });
+  it('Home route loads Home component', () => {
+    expect(routes[0].components.default.name).toBe('HomeView');
+  });
 
-    it("About route loads About component", (done) => {
-        // Component is lazy loaded so need to invoke the import
-        const componentPromise = (routes[1].components.default as () => Promise<any>)();
-        componentPromise.then(
-            (component: any) => {
-                expect(component.default.name).toBe("AboutView");
-                expect(component.default.components).toHaveProperty("VersionInfo")
-                done();
-            }
-        );
-    });
+  it('About route loads About component', (done) => {
+    // Component is lazy loaded so need to invoke the import
+    const componentPromise = (routes[1].components.default as () => Promise<any>)();
+    componentPromise.then(
+      (component: any) => {
+        expect(component.default.name).toBe('AboutView');
+        expect(component.default.components).toHaveProperty('VersionInfo');
+        done();
+      },
+    );
+  });
 });
--- app/client/tests/unit/store/index.spec.ts
+++ app/client/tests/unit/store/index.spec.ts
@@ -1,20 +1,19 @@
-import index from '@/store/index'
+import index from '@/store/index';
 
-describe("store index", () => {
-    
-    it("creates a root state", () => {
-        expect(index).toHaveProperty("state");
-    });
+describe('store index', () => {
+  it('creates a root state', () => {
+    expect(index).toHaveProperty('state');
+  });
 
-    it("has mutations", () => {
-        expect(index).toHaveProperty("_modules.root._rawModule.mutations.addFile");
-        expect(index).toHaveProperty("_modules.root._rawModule.mutations.setVersions");
-        expect(index).toHaveProperty("_modules.root._rawModule.mutations.setUser");
-    });
+  it('has mutations', () => {
+    expect(index).toHaveProperty('_modules.root._rawModule.mutations.addFile');
+    expect(index).toHaveProperty('_modules.root._rawModule.mutations.setVersions');
+    expect(index).toHaveProperty('_modules.root._rawModule.mutations.setUser');
+  });
 
-    it("has actions", () => {
-        expect(index).toHaveProperty("_modules.root._rawModule.actions.processFiles");
-        expect(index).toHaveProperty("_modules.root._rawModule.actions.getVersions");
-        expect(index).toHaveProperty("_modules.root._rawModule.actions.getUser");
-    });
+  it('has actions', () => {
+    expect(index).toHaveProperty('_modules.root._rawModule.actions.processFiles');
+    expect(index).toHaveProperty('_modules.root._rawModule.actions.getVersions');
+    expect(index).toHaveProperty('_modules.root._rawModule.actions.getUser');
+  });
 });
