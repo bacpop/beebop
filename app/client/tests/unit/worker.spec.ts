e58cd58b5b851cc9f1f1605b68de8c2d026767ed
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-07-21 13:24:17 +0100

    adapt unit and e2e tests to changes

--- app/client/tests/unit/worker.spec.ts
+++ app/client/tests/unit/worker.spec.ts
@@ -1,26 +1,57 @@
 import fs from 'fs';
 import vm from 'vm';
+import { moduleMock, AMRprediction, WebSketch } from './wasmMocks';
 
 const data = fs.readFileSync('./public/worker.js', { encoding: 'utf8' });
 const script = new vm.Script(data);
-const worker: Partial<Worker> = {
+interface Others {
+  log: any,
+  moduleMock: any,
+  AMRprediction: any,
+  WebSketch: any
+}
+
+const worker: Partial<Worker> & Others = {
   onmessage: () => {},
   postMessage: jest.fn(),
+  log: console.log,  // eslint-disable-line
+  moduleMock,
+  AMRprediction,
+  WebSketch,
 };
 script.runInNewContext(worker);
 
 describe('Worker', () => {
   const message = {
-    data: { hash: 'abc123' },
+    data: { hash: 'abc123', fileObject: { name: 'mockfile.fa' } },
   };
 
-  it('sends results back', async () => {
-    (worker.onmessage as any)(message);
-    expect(worker.postMessage).toHaveBeenCalledWith(
-      { hash: 'abc123', type: 'amr', result: 'AMR' },
-    );
-    expect(worker.postMessage).toHaveBeenCalledWith(
-      { hash: 'abc123', type: 'sketch', result: 'Sketch' },
-    );
+  (worker.onmessage as any)(message);
+
+  it('creates working dir in FS', (done) => {
+    setTimeout(() => {
+      expect(worker.moduleMock.workdir).toBe('/working');
+      done();
+    });
+  });
+
+  it('puts file into workdir of FS', (done) => {
+    setTimeout(() => {
+      expect(worker.moduleMock.data.filedata).toEqual({ files: [message.data.fileObject] });
+      expect(worker.moduleMock.data.dir).toBe(worker.moduleMock.workdir);
+      done();
+    });
+  });
+
+  it('sends results back', (done) => {
+    setTimeout(() => {
+      expect(worker.postMessage).toHaveBeenCalledWith(
+        { hash: 'abc123', type: 'amr', result: '/working/mockfile.fa' },
+      );
+      expect(worker.postMessage).toHaveBeenCalledWith(
+        { hash: 'abc123', type: 'sketch', result: '/working/mockfile.fa' },
+      );
+      done();
+    });
   });
 });
