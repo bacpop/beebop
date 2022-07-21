47d5f9126c72da66284defab34ef729b313e2227
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-07-21 13:19:57 +0100

    added wasm to webworker

--- app/client/public/worker.js
+++ app/client/public/worker.js
@@ -1,6 +1,29 @@
+if ('function' === typeof importScripts) {
+  importScripts('./web_amr_prediction.js');
+  importScripts('./web_sketch.js');
+}
+
 onmessage = function (message) {
-  const amr_result = "AMR"
-  postMessage({ hash: message.data.hash, type: 'amr', result: amr_result });
-  const sketch_result = "Sketch"
-  postMessage({ hash: message.data.hash, type: 'sketch', result: sketch_result }); 
+  // run amr prediction
+  AMRprediction().then(module => {
+    var f = message.data.fileObject;
+    // create working directory in filesystem
+    module.FS.mkdir('/working');
+    // add file to work dir
+    module.FS.mount(module.FS.filesystems.WORKERFS, { files: [f] }, '/working');
+    // make prediction
+    var amr_result = module.make_prediction("/working/" + f.name);
+    // return result
+    postMessage({ hash: message.data.hash, type: 'amr', result: amr_result });
+  });
+
+  // same for sketch
+  WebSketch().then(module => {
+    const f = message.data.fileObject;
+    module.FS.mkdir('/working');
+    module.FS.mount(module.FS.filesystems.WORKERFS, { files: [f] }, '/working');
+    const sketch = module.sketch('/working/' + f.name, 14, 29, 3, 14, 156, false, true)
+    postMessage({ hash: message.data.hash, type: 'sketch', result: sketch});
+  });
+
 };
