8de58834aaf3b5352aa373ec5236c69ad3d21dd2
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-08-25 17:24:46 +0100

    adapted states and types

--- app/client/tests/mocks.ts
+++ app/client/tests/mocks.ts
@@ -1,11 +1,15 @@
 import { RootState } from '@/store/state';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
+import {
+  ResponseFailure, ResponseSuccess, BeebopError,
+} from '../src/types';
 
 export const mockAxios = new MockAdapter(axios);
 
 export function mockRootState(state: Partial<RootState> = {}): RootState {
   return {
+    errors: [],
     versions: [],
     user: null,
     results: { perIsolate: {} },
@@ -18,3 +22,17 @@ export function mockRootState(state: Partial<RootState> = {}): RootState {
     ...state,
   };
 }
+
+export const mockSuccess = (data: any): ResponseSuccess => ({
+  data,
+  status: 'success',
+  errors: null,
+});
+
+export const mockError = (errorMessage = 'some message'): BeebopError => ({ error: 'OTHER_ERROR', detail: errorMessage });
+
+export const mockFailure = (errorMessage: string): ResponseFailure => ({
+  data: null,
+  status: 'failure',
+  errors: [mockError(errorMessage)],
+});
