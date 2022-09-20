c6225b4294bcf7fd6e6f9b4430069e01afa2cf67
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2022-09-20 14:12:56 +0100

    new component for network visualisation

---
+++ app/client/src/components/NetworkVisualisation.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div id='cy'></div>
+  </div>
+</template>
+
+<script lang='ts'>
+/* eslint-disable no-multi-spaces */
+import { defineComponent } from 'vue';
+import cytoscape from 'cytoscape';
+import graphml from 'cytoscape-graphml';
+import jquery from 'jquery';
+import { mapState, mapActions } from 'vuex';
+
+export default defineComponent({
+  name: 'NetworkVisualisation',
+  data() {
+    return {
+      cluster: 24,
+    };
+  },
+  mounted() {
+    graphml(cytoscape, jquery);
+    this.drawGraph();
+  },
+  computed: {
+    ...mapState(['results']),
+  },
+  methods: {
+    ...mapActions(['getGraphml']),
+    async drawGraph() {
+      if (!this.results.perCluster[this.cluster] || !this.results.perCluster[this.cluster].graph) {
+        await this.getGraphml(this.cluster);
+      }
+      // const graphmlFile = await fetch('./network.graphml');
+      // const graph = await graphmlFile.text();
+      const { graph } = this.results.perCluster[this.cluster];
+      const cy = cytoscape({
+        container: document.getElementById('cy'),
+        style: [
+          {
+            selector: 'node',
+            style: {
+              width: '10px',
+              height: '10px',
+              content: 'data(key0)',
+              'font-size': '7px',
+              'background-color': 'darkblue',
+              'border-style': 'solid',
+              'border-color': 'black',
+              'border-width': '1px',
+            },
+          },
+          {
+            selector: 'edge',
+            style: {
+              width: '2px',
+              'line-color': 'steelblue',
+            },
+          },
+        ],
+        layout: {
+          name: 'grid',
+        },
+      });
+      cy.ready(() => {
+        (cy as any).graphml({ layoutBy: 'cose' }); // eslint-disable-line @typescript-eslint/no-explicit-any
+        (cy as any).graphml(graph); // eslint-disable-line @typescript-eslint/no-explicit-any
+      });
+    },
+  },
+});
+</script>
+<style lang='scss'>
+#cy {
+  height: 600px;
+  width: 960px;
+  border: solid black;
+  text-align: start;
+}
+</style>
