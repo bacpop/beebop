5759fb4eb25d84b25f8207eb3b1161ace724ef34
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-01-16 19:28:38 +0000

    integration test

--- app/server/tests/integration/persistence.test.ts
+++ app/server/tests/integration/persistence.test.ts
@@ -1,4 +1,4 @@
-import {get, post, flushRedis, getRedisValues} from "./utils";
+import {get, post, flushRedis, getRedisHash, getRedisList, saveRedisHash, saveRedisList} from "./utils";
 describe("User persistence", () => {
     let connectionCookie = "";
     beforeEach(async () => {
@@ -14,10 +14,26 @@ describe("User persistence", () => {
             projectName: "test name"
         };
         await post("poppunk", payload, connectionCookie);
-        const mapping = await getRedisValues("beebop:user:hash");
-        expect(Object.keys(mapping).length).toBe(1);
-        expect(mapping["mock:1234"]).toBe("9876");
-        const nameMapping = await getRedisValues("beebop:userproject:name");
-        expect(nameMapping["mock:1234:9876"]).toBe("test name");
+        const userHashes = await getRedisList("beebop:user:hashes:mock:1234");
+        expect(userHashes).toStrictEqual(["9876"]);
+        const projectDetails = await getRedisHash("beebop:userproject:mock:1234:9876");
+        expect(projectDetails).toStrictEqual({name: "test name"});
+    });
+
+    it("gets user project details from redis", async () => {
+        await saveRedisList("beebop:user:hashes:mock:1234", ["abcd", "efgh"]);
+        await saveRedisHash("beebop:userproject:mock:1234:abcd", {name: "test save 1"});
+        await saveRedisHash("beebop:userproject:mock:1234:efgh", {name: "test save 2"});
+
+        const response = await get("projects", connectionCookie);
+        expect(response.status).toBe(200);
+        expect(response.data).toStrictEqual({
+            status: "success",
+            data: [
+                {hash: "abcd", name: "test save 1"},
+                {hash: "efgh", name: "test save 2"}
+            ],
+            errors: []
+        });
     });
 });
