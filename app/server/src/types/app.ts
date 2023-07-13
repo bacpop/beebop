373ab3f78be5dff198cc55034f1d46c338c33e32
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-07-13 15:01:16 +0100

    lint

--- app/server/src/types/app.ts
+++ app/server/src/types/app.ts
@@ -1,5 +1,6 @@
 import {Application} from "express";
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export type BeebopConfig = Record<string, any>;
 export interface BeebopRoutes {
     addRoutes: (app: Application, config: BeebopConfig) => void;
