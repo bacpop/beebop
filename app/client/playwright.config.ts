7bd5589d7f970825bf21121cdf69eaf5277c62a3
EmmaLRussell <EmmaRussellKilburn@gmail.com>
2023-08-02 11:15:36 +0100

    add timeout

--- app/client/playwright.config.ts
+++ app/client/playwright.config.ts
@@ -13,7 +13,7 @@ const config: PlaywrightTestConfig = {
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 10000
+    timeout: 20000
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
