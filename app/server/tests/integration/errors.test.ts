3e5e48aebd618bd3266d5f7d7b363f3098a70f7f
anmol thapar <mr.anmolthapar@gmail.com>
2024-02-23 12:31:36 +0000

    Refactor error handling in integration tests

--- app/server/tests/integration/errors.test.ts
+++ app/server/tests/integration/errors.test.ts
@@ -119,15 +119,14 @@ describe("Error handling", () => {
         }]);
     });
 
-    it.only("Omits unexpected error detail from response", async () => {
+    it("Omits unexpected error detail from response", async () => {
         // Run a project then sabotage its AMR in the database so we get an unexpected JSON parse error
         const projectId = await runProjectToCompletion();
         const redisKey = `beebop:project:${projectId}:sample:${sampleId}`
         await saveRedisHash(redisKey, {"amr": "{{{{nope"});
 
         const projectRes = await get(`project/${projectId}`, connectionCookie);
-        console.log(projectRes.data.data);
-        
+                
         expect(projectRes.status).toBe(500);
         expect(projectRes.data.data).toBe(null);
         expect(projectRes.data.errors.length).toBe(1);
