617cb6e983f68231916a3b827b30202610d541b3
anmol thapar <mr.anmolthapar@gmail.com>
2024-03-06 11:58:43 +0000

    test: router tests

---
+++ app/client-v2/src/__tests__/router.spec.ts
@@ -0,0 +1,79 @@
+import router from "@/router";
+import { useUserStore } from "@/stores/userStore";
+import { createTestingPinia } from "@pinia/testing";
+import { flushPromises } from "@vue/test-utils";
+
+describe("Router", () => {
+  it("should get user and not redirect on page that does not require auth", async () => {
+    const testPinia = createTestingPinia();
+    const userStore = useUserStore(testPinia);
+
+    router.push("/login");
+    await router.isReady();
+
+    expect(userStore.getUser).toHaveBeenCalled();
+  });
+  it("should redirect to login page if user is not authenticated and page requires auth", async () => {
+    const testPinia = createTestingPinia();
+    const userStore = useUserStore(testPinia);
+    // @ts-expect-error getter is readonly
+    userStore.isAuthenticated = false;
+
+    router.push("/");
+    await router.isReady();
+
+    expect(router.currentRoute.value.path).toBe("/login");
+  });
+  it("should not redirect to login page if user is authenticated and page requires auth", async () => {
+    const testPinia = createTestingPinia();
+    const userStore = useUserStore(testPinia);
+    // @ts-expect-error getter is readonly
+    userStore.isAuthenticated = true;
+
+    router.push("/");
+    await router.isReady();
+
+    await flushPromises();
+
+    expect(router.currentRoute.value.path).toBe("/");
+  });
+  it("should be created with correct configuration", () => {
+    expect(router).toBeDefined();
+    expect(router.getRoutes()).toHaveLength(5);
+  });
+
+  it("should define a NotFound route", () => {
+    const route = router.getRoutes().find((route) => route?.name === "NotFound");
+
+    expect(route).toBeDefined();
+    expect(route?.meta.requiresAuth).toBeUndefined();
+  });
+
+  it("should define a home route", () => {
+    const route = router.getRoutes().find((route) => route?.name === "home");
+
+    expect(route).toBeDefined();
+    expect(route?.meta.requiresAuth).toBe(true);
+  });
+
+  it("should define an about route", () => {
+    const route = router.getRoutes().find((route) => route?.name === "about");
+
+    expect(route?.meta.requiresAuth).toBeUndefined();
+    expect(route).toBeDefined();
+  });
+
+  it("should define a project route", () => {
+    const route = router.getRoutes().find((route) => route?.name === "project");
+
+    expect(route).toBeDefined();
+    expect(route?.meta.requiresAuth).toBe(true);
+  });
+
+  it("should define a login route", () => {
+    const route = router.getRoutes().find((route) => route?.name === "login");
+
+    expect(route?.meta.requiresAuth).toBeUndefined();
+    expect(route).toBeDefined();
+  });
+});
