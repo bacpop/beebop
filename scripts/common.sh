0a85e83544768cf891cf70183dc994a8a834234e
anmol thapar <mr.anmolthapar@gmail.com>
2024-03-14 13:26:43 +0000

    Delete unused common script

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,3 +1,3 @@
-// TODO: update to main before merging into main.. Also need to merge beebop_py first
+# TODO: change back to main before merge into main
 export API_BRANCH="bacpop-160-network-tab"
 export DB_LOCATION="./storage/GPS_v6_references"
