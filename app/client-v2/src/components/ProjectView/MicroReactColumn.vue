7c7b41254f8d8a69670fbb162f32af4271428aca
anmol thapar <mr.anmolthapar@gmail.com>
2024-03-15 08:19:50 +0000

    Refactor MicroReactColumn component

--- app/client-v2/src/components/ProjectView/MicroReactColumn.vue
+++ app/client-v2/src/components/ProjectView/MicroReactColumn.vue
@@ -1,89 +1,58 @@
 <script setup lang="ts">
-import { getApiUrl } from "@/config";
+import { useMicroreact } from "@/composables/useMicroreact";
 import { useProjectStore } from "@/stores/projectStore";
-import { useUserStore } from "@/stores/userStore";
-import { type ProjectSample, AnalysisType, type ApiResponse } from "@/types/projectTypes";
-import { mande } from "mande";
-import { ref } from "vue";
+import { type ProjectSample, AnalysisType } from "@/types/projectTypes";
+import Toast from "primevue/toast";
 
 const props = defineProps<{
   data: ProjectSample;
 }>();
 // todo: make into composable & clean up logic
-const microReactApi = mande(`${getApiUrl()}/microreactURL`, { credentials: "include" });
 const projectStore = useProjectStore();
-const userStore = useUserStore();
-const isMicroReactDialogVisible = ref(false);
-const microReactTokenInput = ref("");
-const microReactError = ref(false);
-
-const onMicroReactVisit = async () => {
-  if (userStore.microreactToken) {
-    try {
-      const res = await microReactApi.post<ApiResponse<{ cluster: string; url: string }>>({
-        cluster: props.data.cluster,
-        apiToken: userStore.microreactToken,
-        projectHash: projectStore.project.hash
-      });
-      window.open(res.data.url, "_blank");
-    } catch (error) {
-      console.error(error);
-    }
-  } else {
-    isMicroReactDialogVisible.value = true;
-  }
-};
-
-const saveMicroreactToken = async () => {
-  if (microReactTokenInput.value) {
-    try {
-      const res = await microReactApi.post<ApiResponse<{ cluster: string; url: string }>>({
-        cluster: props.data.cluster,
-        apiToken: microReactTokenInput.value,
-        projectHash: projectStore.project.hash
-      });
-
-      isMicroReactDialogVisible.value = false;
-      userStore.microreactToken = microReactTokenInput.value;
-
-      window.open(res.data.url, "_blank");
-    } catch (error) {
-      microReactError.value = true;
-    }
-  }
-};
+const { hasMicroReactError, isMicroReactDialogVisible, microReactTokenInput, onMicroReactVisit, saveMicroreactToken } =
+  useMicroreact();
 </script>
 
 <template>
+  <Toast />
   <Dialog
+    v-if="props.data.cluster"
     v-model:visible="isMicroReactDialogVisible"
     modal
     :draggable="false"
     header="Submit Microreact Token"
     :style="{ width: '30rem' }"
+    :class="{ 'border-red-500': hasMicroReactError }"
   >
     <span class="p-text-secondary block mb-4">
-      Please submit a Microreact token so a url can be generated.<br />
+      Please submit a Microreact token so a URL can be generated.<br />
       You can find your token in your
-      <a href="https://microreact.org/my-account/settings" target="_blank"> Microreact Account Settings</a>.</span
+      <a
+        class="text-primary no-underline hover:underline font-semibold"
+        href="https://microreact.org/my-account/settings"
+        target="_blank"
+      >
+        Microreact Account Settings</a
+      >.</span
     >
     <div class="flex flex-column gap-2 mb-3">
-      <label for="microreact token" class="font-semibold">Token</label>
+      <label for="microreact token" class="font-medium">Token</label>
       <InputText
         v-model="microReactTokenInput"
         id="microreact token"
         class="flex-auto"
         autocomplete="off"
-        :invalid="microReactError"
+        :invalid="hasMicroReactError"
         aria-errormessage="token-error"
+        placeholder="Enter token..."
       />
-      <small v-if="microReactError" id="token-error" class="text-red-500"
-        >Incorrect token, ensure it is correct and try again</small
+      <small v-if="hasMicroReactError" id="token-error" class="text-red-500"
+        >An error occurred, ensure the token is correct and try again</small
       >
     </div>
     <div class="flex justify-content-end gap-2">
       <Button type="button" label="Cancel" severity="secondary" @click="isMicroReactDialogVisible = false"></Button>
-      <Button type="button" label="Save" @click="saveMicroreactToken"></Button>
+      <Button type="button" label="Save & Visit" @click="saveMicroreactToken(props.data.cluster)"></Button>
     </div>
   </Dialog>
   <div v-if="projectStore.project.status?.microreact === 'finished'" class="flex gap-2">
@@ -100,7 +69,7 @@ const saveMicroreactToken = async () => {
       outlined
       icon="pi pi-arrow-right"
       label="Visit"
-      @click="props.data.cluster && onMicroReactVisit()"
+      @click="props.data.cluster && onMicroReactVisit(props.data.cluster)"
       :disabled="!props.data.cluster"
     />
   </div>
---
+++ app/client-v2/src/composables/useMicroreact.ts
@@ -0,0 +1,67 @@
+import { getApiUrl } from "@/config";
+import { useProjectStore } from "@/stores/projectStore";
+import { useUserStore } from "@/stores/userStore";
+import type { ApiResponse } from "@/types/projectTypes";
+import { mande } from "mande";
+import { useToast } from "primevue/usetoast";
+import { ref } from "vue";
+
+export const useMicroreact = () => {
+  const microReactApi = mande(`${getApiUrl()}/microreactURL`, { credentials: "include" });
+  const projectStore = useProjectStore();
+  const userStore = useUserStore();
+  const isMicroReactDialogVisible = ref(false);
+  const microReactTokenInput = ref("");
+  const hasMicroReactError = ref(false);
+  const toast = useToast();
+
+  const onMicroReactVisit = async (cluster: number) => {
+    if (userStore.microreactToken) {
+      try {
+        const res = await microReactApi.post<ApiResponse<{ cluster: string; url: string }>>({
+          cluster,
+          apiToken: userStore.microreactToken,
+          projectHash: projectStore.project.hash
+        });
+        window.open(res.data.url, "_blank");
+      } catch (error) {
+        console.error(error);
+        toast.add({
+          severity: "error",
+          summary: "Error Occurred",
+          detail: "Try again later or update your Microreact token.",
+          life: 3000
+        });
+      }
+    } else {
+      isMicroReactDialogVisible.value = true;
+    }
+  };
+
+  const saveMicroreactToken = async (cluster: number) => {
+    if (microReactTokenInput.value) {
+      try {
+        const res = await microReactApi.post<ApiResponse<{ cluster: string; url: string }>>({
+          cluster,
+          apiToken: microReactTokenInput.value,
+          projectHash: projectStore.project.hash
+        });
+
+        isMicroReactDialogVisible.value = false;
+        userStore.microreactToken = microReactTokenInput.value;
+        window.open(res.data.url, "_blank");
+      } catch (error) {
+        console.log(error);
+        hasMicroReactError.value = true;
+      }
+    }
+  };
+
+  return {
+    onMicroReactVisit,
+    saveMicroreactToken,
+    isMicroReactDialogVisible,
+    microReactTokenInput,
+    hasMicroReactError
+  };
+};
