02721f96e7ff0d35ff09887c4e55e59f66422544
anmol thapar <mr.anmolthapar@gmail.com>
2024-03-20 08:52:48 +0000

    feat: about page

--- app/client-v2/src/views/AboutView.vue
+++ app/client-v2/src/views/AboutView.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="p-5 mb-3 border-round-sm surface-card">About Page</div>
+  <div class="p-5 mb-3 border-round surface-card shadow-2" style="max-width: 900px">
+    <h1 class="text-2xl font-semibold">About</h1>
+    <p class="text-lg">
+      BeeBOP is being developed by the RESIDE group within MRC GIDA as an online interface to the PopPUNK method
+      developed by John Lees (EMBL-EBI) and Nick Croucher (Imperial College London).<br />
+      This work is part of the Global Pneumococcal Sequencing project, led by Stephen Bentley (Wellcome Sanger
+      Institute) and Lesley McGee (CDC), managed by Stephanie Lo (Wellcome Sanger Institute), and funded by the Bill &
+      Melinda Gates Foundation and Welcome.
+    </p>
+  </div>
 </template>
 
 <style scoped>
