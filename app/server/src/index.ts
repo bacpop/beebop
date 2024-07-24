c5a8a2e84fcc476175f0e4465dc83e2c6057c83c
anmol thapar <mr.anmolthapar@gmail.com>
2024-07-24 08:25:10 +0100

    chore: Update environment variable configuration

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -24,11 +24,11 @@ const configText = fs.readFileSync(filename, { encoding: "utf-8" });
 dotenv.config();
 
 const config = {
-  server_port: process.env.PORT || 4000,
-  api_url: process.env.API_URL || "http://localhost:5000",
-  client_url: process.env.CLIENT_URL || "http://localhost:5173",
-  server_url: process.env.SERVER_URL || "http://localhost:4000",
-  redis_url: process.env.REDIS_URL || "redis://localhost:6379",
+  server_port: process.env.PORT,
+  api_url: process.env.API_URL,
+  client_url: process.env.CLIENT_URL,
+  server_url: process.env.SERVER_URL,
+  redis_url: process.env.REDIS_URL,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   SESSION_SECRET: process.env.SESSION_SECRET,
