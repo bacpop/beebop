6a4041fc43e7bbc176fc545ce7ba1be449cabe52
anmol thapar <mr.anmolthapar@gmail.com>
2024-07-24 08:22:41 +0100

    chore: Update environment variable configuration

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -20,7 +20,22 @@ if (!fs.existsSync(filename)) {
 }
 
 const configText = fs.readFileSync(filename, { encoding: "utf-8" });
-const config = JSON.parse(configText);
+// const config = JSON.parse(configText);
+dotenv.config();
+
+const config = {
+  server_port: process.env.PORT || 4000,
+  api_url: process.env.API_URL || "http://localhost:5000",
+  client_url: process.env.CLIENT_URL || "http://localhost:5173",
+  server_url: process.env.SERVER_URL || "http://localhost:4000",
+  redis_url: process.env.REDIS_URL || "redis://localhost:6379",
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  SESSION_SECRET: process.env.SESSION_SECRET || "beebop-is-great",
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+};
+
 if (!fs.existsSync(filename)) {
     throw new Error(`File ${configPath} does not exist`);
 }
