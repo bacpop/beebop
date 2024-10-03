2add392f4404951f7a84055b42d9a775201fd555
anmol thapar <mr.anmolthapar@gmail.com>
2024-10-03 14:27:18 +0100

    refactor: Update CreateProjectButton.vue to use reactive form object
    - Remove unused import in CreateProjectButton.vue
    - Update projectCsvUtils to include species in BaseProjectInfo
    - Update projectCsvUtils to use consistent capitalization for headers

--- app/client-v2/src/components/HomeView/CreateProjectButton.vue
+++ app/client-v2/src/components/HomeView/CreateProjectButton.vue
@@ -9,35 +9,36 @@ import { useRouter } from "vue-router";
 const props = defineProps<{
   projects: ProjectOverview[];
 }>();
-
+const initialFormState = {
+  name: "",
+  species: null,
+  errors: {
+    name: "",
+    species: "",
+    creation: ""
+  }
+};
+const form = reactive(structuredClone(initialFormState));
 const { showErrorToast } = useToastService();
 const router = useRouter();
 const apiUrl = getApiUrl();
 const visible = ref(false);
-const species = ref(null);
-const name = ref("");
-const errors = reactive({
-  name: "",
-  species: "",
-  creation: ""
-});
 
 const isDuplicateName = (name: string) => props.projects.some((project) => project.name === name);
 
 const closeDialog = () => {
   visible.value = false;
-  species.value = null;
-  errors.name = errors.species = errors.creation = name.value = "";
+  Object.assign(form, structuredClone(initialFormState));
 };
 const validateInputs = () => {
-  errors.species = species.value ? "" : "Species is required";
-  errors.name = name.value ? "" : "Name is required";
+  form.errors.species = form.species ? "" : "Species is required";
+  form.errors.name = form.name ? "" : "Name is required";
 
-  if (isDuplicateName(name.value)) {
-    errors.name = "Name already exists";
+  if (isDuplicateName(form.name)) {
+    form.errors.name = "Name already exists";
   }
 
-  return !errors.species && !errors.name;
+  return !form.errors.species && !form.errors.name;
 };
 
 const createProject = async () => {
@@ -48,12 +49,12 @@ const createProject = async () => {
   const { error, data: project } = await useFetch(apiUrl + "/project", {
     credentials: "include"
   })
-    .post({ name: name.value, species: species.value })
+    .post({ name: form.name, species: form.species })
     .json();
 
   if (error.value) {
-    errors.creation = "Error creating project";
-    showErrorToast(errors.creation);
+    form.errors.creation = "Error creating project";
+    showErrorToast(form.errors.creation);
     return;
   }
 
@@ -71,18 +72,18 @@ const createProject = async () => {
     modal
     header="Create Project"
     :style="{ width: '25rem' }"
-    :class="{ 'border-red-500': errors.creation }"
+    :class="{ 'border-red-500': form.errors.creation }"
   >
     <div class="mb-4 p-text-secondary">Create new project for a given species</div>
     <div class="flex flex-column gap-2 mb-4">
       <label for="species" class="font-semibold">Species</label>
-      <Dropdown v-model="species" :options="SPECIES" placeholder="Select a Species" class="w-full" />
-      <small v-if="!!errors.species" id="species-error" class="text-red-500">{{ errors.species }}</small>
+      <Dropdown v-model="form.species" :options="SPECIES" placeholder="Select a species" class="w-full" />
+      <small v-if="!!form.errors.species" id="species-error" class="text-red-500">{{ form.errors.species }}</small>
     </div>
     <div class="flex flex-column gap-2 mb-4">
       <label for="name" class="font-semibold">Name</label>
-      <InputText id="name" class="w-full" autocomplete="off" v-model="name" @keydown.enter="createProject" />
-      <small v-if="!!errors.name" id="name-error" class="text-red-500">{{ errors.name }}</small>
+      <InputText id="name" class="w-full" autocomplete="off" v-model="form.name" @keydown.enter="createProject" />
+      <small v-if="!!form.errors.name" id="name-error" class="text-red-500">{{ form.errors.name }}</small>
     </div>
     <div class="flex justify-content-end gap-2">
       <Button type="button" label="Cancel" severity="secondary" @click="closeDialog"></Button>
