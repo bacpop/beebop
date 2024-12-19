f047471a52b86b08c27a8b50e801d1e0fcb4beb9
anmol thapar <mr.anmolthapar@gmail.com>
2024-12-19 12:06:01 +0000

    fix: swap info messages in NetworkTab for clarity on graph viewing options

--- app/client-v2/src/components/ProjectView/NetworkTab.vue
+++ app/client-v2/src/components/ProjectView/NetworkTab.vue
@@ -21,11 +21,11 @@ const { data, error, isFetching } = useFetch(`${apiUrl}/networkGraphs/${store.pr
   </div>
   <div v-else-if="data?.data">
     <InlineMessage severity="info" class="mb-2"
-      >View in fullscreen, reset layout or use mouse, touchpad or touchscreen gestures on graphs to zoom in and out, or
-      move nodes around.</InlineMessage
+      >These graphs are pruned versions of the full graphs. To view full graphs, download the zip file and view the .graphml externally.</InlineMessage
     >
     <InlineMessage severity="info" class="mb-2"
-      >Note: These graphs are pruned versions of the full graphs. To view full graphs, download the zip file and view the .graphml externally.</InlineMessage
+      >View in fullscreen, reset layout or use mouse, touchpad or touchscreen gestures on graphs to zoom in and out, or
+      move nodes around.</InlineMessage
     >
     <div class="grid">
       <div v-for="(value, key) in data.data" :key="key" class="col">
